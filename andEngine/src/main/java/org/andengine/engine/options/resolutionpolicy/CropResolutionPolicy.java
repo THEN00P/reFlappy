a15ac4d96eca4b8acd1a30e174a31c80ede65685
THEN00P <lorenzvlg@gmail.com>
2025-06-29 16:23:26 +0200

    make CropResolutionPolicy fit andengine version

--- andEngine/src/main/java/org/andengine/engine/options/resolutionpolicy/CropResolutionPolicy.java
+++ andEngine/src/main/java/org/andengine/engine/options/resolutionpolicy/CropResolutionPolicy.java
@@ -1,12 +1,11 @@
 package org.andengine.engine.options.resolutionpolicy;
 
-import org.andengine.engine.options.resolutionpolicy.BaseResolutionPolicy;
-
-import android.view.View.MeasureSpec;
+import android.view.View;
+import org.andengine.opengl.view.RenderSurfaceView;
 
 /**
  * Based on CroppedResolutionPolicy by jgibbs, added methods to get corners coordinates
- * http://www.andengine.org/forums/gles2/targeting-multiple-display-resolutions-t6794.html#p29955
+ * https://web.archive.org/web/20140415202340/http://www.andengine.org/forums/gles2/targeting-multiple-display-resolutions-t6794.html
  *
  * @author jgibbs, Martin Varga
  *
@@ -29,46 +28,33 @@ public class CropResolutionPolicy extends BaseResolutionPolicy {
         desiredHeight = pHeight;
     }
 
-    @Override
-    public void onMeasure(Callback pResolutionPolicyCallback,
-                          int pWidthMeasureSpec, int pHeightMeasureSpec) {
-
-        BaseResolutionPolicy.throwOnNotMeasureSpecEXACTLY(pWidthMeasureSpec,
-                pHeightMeasureSpec);
-
-        final int measuredWidth = MeasureSpec.getSize(pWidthMeasureSpec);
-        final int measuredHeight = MeasureSpec.getSize(pHeightMeasureSpec);
-
-        final float desiredRatio = (float) desiredWidth / (float) desiredHeight;
-        float scaleRatio;
-
-        float resultWidth;
+    @Override // org.andengine.engine.options.resolutionpolicy.c
+    /* renamed from: a */
+    public void onMeasure(RenderSurfaceView pRenderSurfaceView, int pWidthMeasureSpec, int pHeightMeasureSpec) {
         float resultHeight;
-
-        if ((float) measuredWidth / (float) measuredHeight < desiredRatio) {
-            // Scale to fit height, width will crop
+        float resultWidth;
+        float scaleRatio;
+        BaseResolutionPolicy.throwOnNotMeasureSpecEXACTLY(pWidthMeasureSpec, pHeightMeasureSpec);
+        int measuredWidth = View.MeasureSpec.getSize(pWidthMeasureSpec);
+        int measuredHeight = View.MeasureSpec.getSize(pHeightMeasureSpec);
+        float desiredRatio = this.desiredWidth / this.desiredHeight;
+        if (measuredWidth / measuredHeight < desiredRatio) {
             resultWidth = measuredHeight * desiredRatio;
             resultHeight = measuredHeight;
-            scaleRatio = desiredHeight / resultHeight;
+            scaleRatio = this.desiredHeight / resultHeight;
         } else {
-            // Scale to fit width, height will crop
             resultHeight = measuredWidth / desiredRatio;
             resultWidth = measuredWidth;
-            scaleRatio = desiredWidth / resultWidth;
+            scaleRatio = this.desiredWidth / resultWidth;
         }
-
-        userWidth = measuredWidth * scaleRatio;
-        userHeight = measuredHeight * scaleRatio;
-
-        left = (desiredWidth - userWidth) / 2f;
-        right = userWidth + left;
-        bottom = (desiredHeight - userHeight) / 2f;
-        top = userHeight + bottom;
-
-        pResolutionPolicyCallback.onResolutionChanged(Math.round(resultWidth), Math.round(resultHeight));
-
+        this.userWidth = measuredWidth * scaleRatio;
+        this.userHeight = scaleRatio * measuredHeight;
+        this.left = (this.desiredWidth - this.userWidth) / 2.0f;
+        this.right = this.userWidth + this.left;
+        this.bottom = (this.desiredHeight - this.userHeight) / 2.0f;
+        this.top = this.userHeight + this.bottom;
+        pRenderSurfaceView.setMeasuredDimensionProxy((int) resultWidth, (int) resultHeight);
     }
-
 
     public float getUserWidth() {
         return userWidth;
@@ -97,6 +83,4 @@ public class CropResolutionPolicy extends BaseResolutionPolicy {
     public float getBottom() {
         return bottom;
     }
-
-
 }
