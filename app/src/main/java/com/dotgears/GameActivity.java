b231c5e7112fda6d81e48022dbe5552bd53a6da2
THEN00P <lorenzvlg@gmail.com>
2025-07-01 01:23:59 +0200

    fullscreen game activity

--- app/src/main/java/com/dotgears/GameActivity.java
+++ app/src/main/java/com/dotgears/GameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
@@ -196,6 +197,15 @@ public class GameActivity extends SimpleBaseGameActivity implements GameHelper.G
     @Override // org.andengine.ui.activity.BaseGameActivity, android.app.Activity
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+		View decorView = getWindow().getDecorView();
+		decorView.setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+						| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_FULLSCREEN
+		);
         this.gameHelper = new GameHelper(this);
         if (this.debugLog) {
             this.gameHelper.enableDebugLog(this.debugLog, this.logTag);
