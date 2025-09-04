52c2974cf31bfc8a49524123eece6cc99a5cc1fc
THEN00P <lorenzvlg@gmail.com>
2025-09-04 20:09:01 +0200

    Release 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = 21
 //        targetSdk = 15
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.1"
+        versionCode = 5
+        versionName = "1.4.0"
     }
 
     buildTypes {
